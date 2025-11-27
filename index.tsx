cb42c1113713c514c08f55d528e5180b38df5d4a
aprendeallah-SYA <aprendeallah@gmail.com>
2025-11-27 09:42:41 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Importamos index.css para que Vite sepa dónde está (Aunque esté vacío)
+import './index.css'; 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
