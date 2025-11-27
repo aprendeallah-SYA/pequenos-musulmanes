96f4a8caa6ee4ebafeefeeeb879d1c09e13e2ace
aprendeallah-SYA <aprendeallah@gmail.com>
2025-11-27 09:24:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ESTA ES LA LÍNEA MÁGICA QUE FALTA:
+      base: '/pequenos-musulmanes/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
